88b3de7c7441f05ecc3bcbcaeb144d8827a26e3c
Trần Trọng Nhất <trongnhat0902@gmail.com>
2022-03-27 19:11:21 +0700

    first commit

---
+++ Blog_baiTap/assets/js/main.js
@@ -0,0 +1,12 @@
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
+var headerHeight = header.clientHeight
+mobileMenu.onclick = function() {
+    var isClose = header.clientHeight === headerHeight;
+    if (isClose) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = '94px';
+
+    }
+}
